4f8bec86d31fd85b50519bd626e9f600160c6bfb
anitkumarsingh <anikumarsingh99@gmail.com>
2021-10-30 13:54:04 +0530

    accessing template and DOM by viewChild decorator

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,7 +8,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
  @Output() serverCreated = new EventEmitter<{serverName:string,serverContent:string}>();
  @Output('bpServer') bluePrintserverCreated = new EventEmitter<{serverName:string,serverContent:string}>();
-  
+ @ViewChild('localServerContent') serverContent:ElementRef 
   // serverName = '';
   // serverContent = '' ;
 
@@ -17,16 +17,16 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  addServers(localServerName:HTMLInputElement,localServerContent:HTMLInputElement){
+  addServers(localServerName:HTMLInputElement){
   this.serverCreated.emit({
     serverName:localServerName.value, // this.serverName
-    serverContent:localServerContent.value // this.serverContent
+    serverContent:this.serverContent.nativeElement.value // this.serverContent
   });
   }
-  bluePrintServers(localServerName:HTMLInputElement,localServerContent:HTMLInputElement){
+  bluePrintServers(localServerName:HTMLInputElement){
     this.bluePrintserverCreated.emit({
       serverName:localServerName.value,
-      serverContent:localServerContent.value
+      serverContent:this.serverContent.nativeElement.value 
     });
   }
 
